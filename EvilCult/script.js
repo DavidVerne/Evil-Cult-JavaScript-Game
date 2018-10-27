d696f906986e2f6fc6689b1dbb0957cfc0ec2f3e
David Verne <david.verne87@gmail.com>
2018-10-27 19:51:40 +0100

    Update script.js

--- EvilCult/script.js
+++ EvilCult/script.js
@@ -1,8 +1,9 @@
-
+// function launches game
 function startgame() {
 
   document.getElementById("gameform").style.display = "none";
 
+// retrieves values from form  
   var firstName = document.getElementById("firstname").value;
   var lastName = document.getElementById("lastname").value;
   var cultName = document.getElementById("cultname").value;
@@ -13,10 +14,10 @@ function startgame() {
     + " You're about to join the ranks of some seriously evil leaders."
     + " Hit OK to begin your cult journey.");
 
-    var resOne = prompt("You currently has 0 followers. You need to change that. A cult leader is nothing without followers."
+   var resOne = prompt("You currently has 0 followers. You need to change that. A cult leader is nothing without followers."
     + " I wonder where you can find some impressionable minds. Here are some suggestions: 1.party 2.college 3.army").toLowerCase();
 
-    switch(resOne) {
+   switch(resOne) {
       case "party":
        alert("You've entered the party and see a group that can be easily persuaded. Go over and convince them to join your cult.");
        break
@@ -34,17 +35,22 @@ function startgame() {
        }
     }
 
-    alert("Well done, you've impressed them enough and they have decided to join. Your new followers are: ");
+   alert("Well done, you've impressed them enough and they have decided to join. Your new followers are: ");
 
-    var followers = ["Jim", "Polly", "Martin", "Jessica", "Brian"];
+   // array of followers
+   var followers = ["Jim", "Polly", "Martin", "Jessica", "Brian"];
 
-    for (var i = 0; i < followers.length; i++) {
+   // loop through followers array
+   for (var i = 0; i < followers.length; i++) {
+      
       alert(followers[i]);
+      
     }
 
     var resTwo = prompt("To keep your followers you'll to keep them inspired. Followers love rituals. Try one of these (enter the number): 1. Sacrifice a cat 2. Read a poem 3. Do a pentagram dance").toLowerCase();
 
     if (resTwo == 1 || 2 || 3) {
+      
       alert("OK Brian has been freaked out and left the cult.");
 
       var followersTwo = followers.pop();
@@ -57,18 +63,24 @@ function startgame() {
 
     }
 
-     alert("Not to worry. The rest of the group are dedicated. Its time to put them to work. Every evil cult leader knows that the key to success in this field is getting your followers to recruit for you. Its called the Fibonacci rule for cult exploitation.")
+    alert("Not to worry. The rest of the group are dedicated. Its time to put them to work. Every evil cult leader knows that the key to success in this field is getting your followers to recruit for you. Its called the Fibonacci rule for cult exploitation.")
 
     alert("Wow your cult has exploded. You now have 126 followers. You've entered the realm of the greats. You've been approached by a documenatry film crew to make a film about your cult.");
 
     var resThree = prompt("The documentary has brought heat and with heat comes the Feds. You're being investigated. It's time to run or face the music. Choose run or fight?").toLowerCase();
 
     if (resThree == "run") {
+      
       alert("You've escaped to Guyana and are safe for now.");
+      
     } else if (resThree == "fight") {
+      
       alert("You've been busted! But your legacy will live on.");
+      
     } else {
+      
       alert("That wasnt an option. But this is the end anyway");
+      
     }
 
     alert("You're adventures have ended for now. Game Over!");
@@ -77,12 +89,19 @@ function startgame() {
 
     var resFive = parseInt(resFour);
 
+  // feedback from user
     if (resFive <= 5) {
+      
       alert("OK. I'll try and improve the game");
+      
     } else if (resFive > 10){
+      
       alert("Wow that's greater than the max");
+      
     } else {
+      
       alert("Awesome, thanks!");
+      
     }
 
   }
